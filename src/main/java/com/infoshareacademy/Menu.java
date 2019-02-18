08b10c2c0977c657bdf8e22008ffef9e206cd60b
Krzysztof Gołacki <golacki.job@gmail.com>
2019-02-18 10:56:07 +0100

    Menu.java_Person.java

--- src/main/java/com/infoshareacademy/Menu.java
+++ src/main/java/com/infoshareacademy/Menu.java
@@ -8,7 +8,7 @@ public class Menu {
     public static void menu() {
         System.out.println("         ********           ");
         System.out.println("******** MENU-APP **********");
-        System.out.println("* 1.  -- Username  --      * ");
+        System.out.println("* 1.  --  Person  --      * ");
         System.out.println("* 2.  -- Post Code --      * ");
         System.out.println("* 3.  --   City    --      * ");
         System.out.println("* 4.  --  Gender   --      * ");
@@ -18,26 +18,20 @@ public class Menu {
         Integer[] tabOfnumebr = {0, 1, 2, 3, 4}; Integer numberOfmenu = scanner.nextInt();
             switch (tabOfnumebr[numberOfmenu]) {
                 case 1:
-                    System.out.println("***********************");
-                    System.out.println("Write your name: ");
-                    Scanner scanner1 = new Scanner(System.in);
-                    String name = scanner1.nextLine();
-                    System.out.println("Your username: " + name);
-                    System.out.println("-----------------------");
+                    Person.writeOfname();
+                    Person.writeOfsurname();
+                    Person.isMaleorFamle();
                 case 2:
-                    System.out.println("**********************");
                     System.out.println("Write your Post Code: ");
                     Scanner scanner2 = new Scanner(System.in);
                     Integer postCode = scanner2.nextInt();
                     System.out.println("Post Code: " + postCode);
-                    System.out.println("-----------------------");
                 case 3:
                     System.out.println("***********************");
                     System.out.println("Write your City: ");
                     Scanner scanner3 = new Scanner(System.in);
                     String city = scanner3.nextLine();
                     System.out.println("City: " + city);
-                    System.out.println("-----------------------");
                 case 0:
                     break;
 
---
+++ src/main/java/com/infoshareacademy/Person.java
@@ -0,0 +1,41 @@
+package com.infoshareacademy;
+
+import java.util.Scanner;
+
+public class Person {
+
+    public String name;
+    public String surname;
+    public String gender;
+    public Integer age;
+
+    public static void writeOfname() { ;
+        Scanner writeOfnameSc = new Scanner(System.in);
+        System.out.println("**********************");
+        System.out.println("Write yours name: ");
+        String name = writeOfnameSc.nextLine();
+        System.out.println("Yours name: " + name);
+    }
+    public static void writeOfsurname() {
+        Scanner writeOfsurnameSc = new Scanner(System.in);
+        System.out.println("**********************");
+        System.out.println("Write yours surname: ");
+        String name = writeOfsurnameSc.nextLine();
+        System.out.println("Yours surname: " + name);
+    }
+
+    public static void isMaleorFamle () {
+        Scanner isMaleorFamle = new Scanner(System.in);
+        System.out.println("**********************");
+        System.out.println("Choice yours Gender: ");
+        String isMaleorFamleSc = isMaleorFamle.nextLine();
+        if (isMaleorFamleSc.equalsIgnoreCase("F")){
+            System.out.println("Famle");
+            System.out.println("**********************");
+        } else if (isMaleorFamleSc.equalsIgnoreCase("M"))
+            System.out.println("Male");
+            System.out.println("**********************");
+
+    }
+
+}
